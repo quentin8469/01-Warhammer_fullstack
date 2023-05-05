1148489e51929cf2192368550cb99ec6818d9b72
Quentin Laparra <quentin.laparra@gmail.com>
2023-05-05 11:07:40 +0200

    WIP test modele player

--- warhammer/tests.py
+++ warhammer/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from account.models import CustomUser
-from warhammer.models import Campagne
+from warhammer.models import Campagne, Player
 
 # Create your tests here.
 
@@ -46,3 +46,66 @@ class CampagneModelTestCase(TestCase):
         self.campagne.delete()
         with self.assertRaises(Campagne.DoesNotExist):
             Campagne.objects.get(nom_de_campagne="Campagne 01")
+
+
+class PlayerModelTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="Jeannot Jojo", password="motdepasse"
+        )
+        self.maitre_du_jeu = CustomUser.objects.create_user(
+            username="Jeannette Jojo", password="motdepassejeanette"
+        )
+        self.campagne = Campagne.objects.create(
+            nom_de_campagne="Campagne 02",
+            date_debut="2023-05-01",
+            date_end="2023-06-01",
+            campagne_etat="En cours",
+            maitre_du_jeu=self.maitre_du_jeu,
+        )
+
+    def test_create_player(self):
+        player = Player.objects.create(
+            nom="TestNomPlayer",
+            race="Nain",
+            sexe="Mâle",
+            vocation="Guerrier",
+            alignement="Loyal",
+            age=30,
+            taille="1m50",
+            poids=80,
+            cheveux="Brun",
+            yeux="Bleus",
+            joueur=self.user,
+            campagne=self.campagne,
+        )
+
+        self.assertEqual(player.nom, "TestNomPlayer")
+        self.assertEqual(player.race, "Nain")
+        self.assertEqual(player.sexe, "Mâle")
+        self.assertEqual(player.vocation, "Guerrier")
+        self.assertEqual(player.alignement, "Loyal")
+        self.assertEqual(player.age, 30)
+        self.assertEqual(player.taille, "1m50")
+        self.assertEqual(player.poids, 80)
+        self.assertEqual(player.cheveux, "Brun")
+        self.assertEqual(player.yeux, "Bleus")
+        self.assertEqual(player.joueur, self.user)
+        self.assertEqual(player.campagne, self.campagne)
+
+    def test_player_str_method(self):
+        player = Player.objects.create(
+            nom="TestNomPlayer",
+            race="Nain",
+            sexe="Mâle",
+            vocation="Guerrier",
+            alignement="Loyal",
+            age=30,
+            taille="1m50",
+            poids=80,
+            cheveux="Brun",
+            yeux="Bleus",
+            joueur=self.user,
+        )
+
+        self.assertEqual(str(player), "TestNomPlayer - Jeannot Jojo -  - ")
